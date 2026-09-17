1b355d7b9cb8ab7cc17a83ff7e051c2530e828c4
sandhiyakumari1606-commits <sandhiyakumari1606@gmail.com>
2026-09-17 21:34:45 +0530

    Create student_management.py

---
+++ student_management.py
@@ -0,0 +1,8 @@
+print("STUDENT MANAGEMENT SYSTEM ")
+name=input("Enter student name:")
+age=input("Enter student age:")
+course=input("Enter student course:")
+print("\n---Students Details---")
+print("Name:",name)
+print ("Age:",age)
+print ("Course:",course)
