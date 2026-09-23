68a4c96654617e6c07f0eb458e24ebdbf854cce3
sandhiyakumari1606-commits <sandhiyakumari1606@gmail.com>
2026-09-23 19:59:43 +0530

    Update student_management.py
    Added student data storage

--- student_management.py
+++ student_management.py
@@ -6,3 +6,10 @@ print("\n---Students Details---")
 print("Name:",name)
 print ("Age:",age)
 print ("Course:",course)
+students=[]
+students.append({
+"name":name,
+"age":age,
+"course":course
+})
+print ("\n Student added successfully!")
